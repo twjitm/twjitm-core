9f88b9d8da9cf00761abed7eee80ef0a2bcde271
twjitm <1029718215@qq.com>
2018-08-17 11:26:56 +0800

    优化日志框架，添加系统相关信息打印

---
+++ src/main/java/com/twjitm/core/common/system/SystemService.java
@@ -0,0 +1,1 @@
+package com.twjitm.core.common.systeam;/** * @author EGLS0807 - [Created on 2018-08-17 10:53] * @company http://www.g2us.com/ * @jdk java version "1.8.0_77" */public class SysteamService {}
