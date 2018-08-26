f4931983d2e4040ed3d23efa55f84fa27a43cb99
twjitm <1029718215@qq.com>
2018-08-26 12:27:20 +0800

    添加工具类

--- src/main/java/com/twjitm/core/utils/spring/BeanUtils.java
+++ src/main/java/com/twjitm/core/utils/spring/BeanUtils.java
@@ -1,10 +1,27 @@
 package com.twjitm.core.utils.spring;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Service;
+
 /**
  * @author EGLS0807 - [Created on 2018-07-27 11:03]
  * @company http://www.g2us.com/
  * @jdk java version "1.8.0_77"
  */
-public class BeanUtils {
+@Service
+public class BeanUtils   implements ApplicationContextAware {
+    private static ApplicationContext ctx;
+    @Override
+    public void setApplicationContext(ApplicationContext arg0)throws BeansException {
+        ctx = arg0;
+    }
 
+    public static Object getBean(String beanName) {
+        if(ctx == null){
+            throw new NullPointerException();
+        }
+        return ctx.getBean(beanName);
+    }
 }
