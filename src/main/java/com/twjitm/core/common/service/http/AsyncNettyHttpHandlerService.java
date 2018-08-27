9a02698f4f0c3846b342a03d162243038eec2649
twjitm <1029718215@qq.com>
2018-08-27 09:26:03 +0800

    添加代码注释

--- src/main/java/com/twjitm/core/common/service/http/AsyncNettyHttpHandlerService.java
+++ src/main/java/com/twjitm/core/common/service/http/AsyncNettyHttpHandlerService.java
@@ -6,6 +6,13 @@ import io.netty.util.concurrent.DefaultEventExecutorGroup;
 import org.springframework.stereotype.Service;
 
 /**
+ *
+ * 当业务代码中存在对EventLoop线程阻塞的时候，ChannelPipeline
+ * 有一些能接收一个EventExecutorGroup的add()方法，如果一个事件
+ * 被专递给自定义的一个EventExecutorGroup，他将会包含在这个EventExecutorGroup
+ * 中的某个EventExecutor所处理，从而被Channel本身的EventLoop移除，对于这种用法
+ * Netty提供了一个种叫做{@link io.netty.util.concurrent.DefaultEventExecutorGroup}的实现
+ * 其实也是本类主要的一个核心API，
  * @author EGLS0807 - [Created on 2018-08-16 12:17]
  * @company http://www.g2us.com/
  * @jdk java version "1.8.0_77"
