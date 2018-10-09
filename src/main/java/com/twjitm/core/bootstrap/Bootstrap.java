23305f98dcdc4aa6ff579c3deff92d31e13ebe83
twjitm <1029718215@qq.com>
2018-10-09 18:52:57 +0800

    添加文档说明和修复启动项bug

--- src/main/java/com/twjitm/core/bootstrap/Bootstrap.java
+++ src/main/java/com/twjitm/core/bootstrap/Bootstrap.java
@@ -39,7 +39,6 @@ public class Bootstrap {
         Bootstrap.startServer();
         AsyncExecutorService asyncExecutorService = SpringServiceManager.getSpringLoadService().getAsyncExecutorService();
         asyncExecutorService.saveUser(null);
-        logger.info("eeee");
     }
 
     public static void init() {
